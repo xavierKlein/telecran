311cdc5e1ff2afd9c129f42f9f0ccf70e97eab77
xavierKlein <xkleink@gmail.com>
2014-05-15 17:17:10 +0200

    first

---
+++ piano2.ino
@@ -0,0 +1,40 @@
+int buttons [6];
+
+int notes[] = {262,294,330,349};
+
+void setup() 
+{
+  buttons [0]=2;
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int keyVal = analogRead(A0);
+  Serial.println(keyVal);
+  
+  if (keyVal == 1023)
+  {
+    tone(12, notes[0]);
+  }
+  
+  else if (keyVal >= 990 && keyVal <= 1010)
+  {
+    tone(12, notes[1]);
+  }
+  
+  else if (keyVal >= 505 && keyVal <= 515)
+  {
+    tone(12, notes[2]);
+  }
+  
+  else if (keyVal >= 5 && keyVal <= 10)
+  {
+    tone(12, notes[3]);
+  }
+  
+  else
+  {
+    noTone(12);
+  }
+}
